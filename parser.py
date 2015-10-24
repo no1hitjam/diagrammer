1d8dec851a4c31dc0bc28870e7f53c369e5af76e
Jackson <jacksonlango@gmail.com>
2015-10-23 22:12:00 -0700

    initial commit

---
+++ parser.py
@@ -0,0 +1,54 @@
+from wordtypes import WT
+#from wordtypes import nodes
+from wordtypes import get_expansions
+
+def print_node_lists(node_lists):
+	for node_list in node_lists:
+		for node in node_list:
+			print str(node) + " "
+		print "\n"
+
+class ParseNode:
+	def __init__(self, word, parent):
+		self.word = word
+		self.parent = parent
+
+def parse_string_lists(input_word_list = [], node_lists = [[]]):
+	idx = 0
+	while idx < len(input_word_list):
+		#print "idx: " + str(idx)
+		# get next string in the input sentence
+		cur_word = input_word_list[idx]
+		# fill str_match_lists with node_lists with matching ParseNoded strings up to the idx.
+		str_match_lists = []
+		print "idx: " + str(idx)
+		while len(node_lists) > 0:
+			#print "len(node_lists): " + str(len(node_lists))
+			#print_node_lists(node_lists)
+			node_list = node_lists.pop()
+			#print "node_list: " + str(node_list) 
+			# check if node_list is too small and needs to be skipped
+			if len(node_list) <= idx: 
+				continue
+			# if the current index is a str, check if it matches the current word
+			if type(node_list[idx]) is str:
+				print node_list[idx] + ", " + cur_word
+				if node_list[idx] == cur_word:	
+					str_match_lists.append(node_list)
+				else:
+					continue
+			
+			# otherwise, expand
+			else:
+				for expansion in get_expansions(node_list[idx]):
+					#print "expansion: " + str(expansion)
+					if idx + 1 < len(node_list):
+						node_lists.append(node_list[:idx] + expansion + node_list[idx + 1:])
+					else:
+						node_lists.append(node_list[:idx] + expansion)
+		# prepare next set of lists
+		node_lists = str_match_lists
+		idx += 1
+	return node_lists
+	
+#print parse_string_lists(["Harry", "and", "Harry", "sat"], [[WT.Sentence]])
---
+++ wordtypes.py
@@ -0,0 +1,45 @@
+from enum import Enum
+
+WT = Enum(
+	'Sentence',
+	'Subject',
+	'Predicate',
+	'Noun'
+)
+
+class WordTypeNode:
+	def __init__(self, parent, expansions):
+		self.parent = parent
+		self.expansions = expansions
+
+def create_dic(wt_expansion_tuple_list):
+	# tuple[0]: wordtype, tuple[1]: expansion
+	nodes = {}
+	for wt_expansion_tuple in wt_expansion_tuple_list:
+		nodes[wt_expansion_tuple[0]] = WordTypeNode(wt_expansion_tuple[0], wt_expansion_tuple[1])
+	return nodes
+	
+	
+nodes = create_dic([
+	( WT.Sentence, [[WT.Subject, WT.Predicate]] ),
+	( WT.Subject, [[WT.Noun], [WT.Noun, "and", WT.Subject], [WT.Noun, "or", WT.Subject]] ),
+	( WT.Noun, [["Harry"]] ),
+	( WT.Predicate, [["sat"]] )
+])
+
+def get_expansions(node):
+	if type(node) is str:
+		return [[node]]
+	else:
+		if node in nodes:
+			return nodes[node].expansions
+		else:
+			return [["Error: word not it nodes dictionary"]]
+
+#nodes = {
+#	WT.Sentence : [[WT.Subject, WT.Predicate]],
+#	WT.Subject : [[WT.Noun], [WT.Noun, "And", WT.Subject], [WT.Noun, "Or", WT.Subject]],
+#	WT.Noun : [["Harry"]],
+#	WT.Predicate : [["sat"]]
+#}
+
